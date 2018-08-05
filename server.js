bf813966acd804822857d49c20064c9a35cb1dec
mirzachilman <mirzachilman@gmail.com>
2018-08-06 06:47:46 +0700

    done

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const Chatkit = require('@pusher/chatkit-server')
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const Chatkit = require("@pusher/chatkit-server");
 
-const app = express()
+const app = express();
 
 const chatkit = new Chatkit.default({
-  instanceLocator: 'v1:us1:ac693f4f-a223-4fb8-9cae-d2de840aaba0',
+  instanceLocator: "v1:us1:ac693f4f-a223-4fb8-9cae-d2de840aaba0",
   key:
-    'f5dc25eb-9547-4d4f-a872-98f2f192d8d0:jPmtjs/iHd1AeJTUCmu+T4L6yb2OPNNfiPta5Cdoi/M='
-})
+    "f5dc25eb-9547-4d4f-a872-98f2f192d8d0:jPmtjs/iHd1AeJTUCmu+T4L6yb2OPNNfiPta5Cdoi/M="
+});
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
 
-app.post('/users', (req, res) => {
-  const { username } = req.body
+app.post("/users", (req, res) => {
+  const { username } = req.body;
   chatkit
     .createUser({
       id: username,
@@ -24,24 +24,21 @@ app.post('/users', (req, res) => {
     })
     .then(() => res.sendStatus(201))
     .catch(error => {
-      if (error.error === 'services/chatkit/user_already_exists') {
-        res.sendStatus(200)
+      if (error.error === "services/chatkit/user_already_exists") {
+        res.sendStatus(200);
       } else {
-        res.status(error.status).json(error)
+        res.status(error.status).json(error);
       }
-    })
-})
+    });
+});
 
-app.post('/authenticate', (req, res) => {
-  const authData = chatkit.authenticate({ userId: req.query.user_id })
-  res.status(authData.status).send(authData.body)
-})
+app.post("/authenticate", (req, res) => {
+  const authData = chatkit.authenticate({ userId: req.query.user_id });
+  res.status(authData.status).send(authData.body);
+});
 
-const PORT = 3001
-app.listen(PORT, err => {
-  if (err) {
-    console.error(err)
-  } else {
-    console.log(`Running on port ${PORT}`)
-  }
-})
+const port = process.env.PORT || 3000;
+// app.listen(port || process.env.PORT, process.env.IP , () => {
+app.listen(port, process.env.IP, () => {
+  console.log(`Server Started on Port ${port}`);
+});
